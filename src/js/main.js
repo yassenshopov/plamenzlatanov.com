96e5da6e820b4c81d68a30142536a9cc1497d4bc
Yassen Shopov <yassenshopov00@gmail.com>
2022-11-14 22:34:11 +0200

    Created JS animation for sliding

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
+let carousels = document.getElementsByClassName("carousel");
+let temp_dict = {}
+for (let i=0; i<carousels.length; i++) {
+    temp_dict[i] = []
+}
+
 function scrollRight() {
-    let carousels = document.getElementsByClassName("carousel");
-    console.log(carousels)
+    // console.log(carousels)
     for (let carousel=0; carousel<carousels.length; carousel++) {
-        console.log(carousels[carousel])
-        carousels[carousel].scrollLeft += 50;
+        console.log(carousels[carousel].scrollLeft)
+        carousels[carousel].scrollLeft += 2;
+        if (!(temp_dict[carousel].includes(carousels[carousel].scrollLeft))) {
+            temp_dict[carousel].push(carousels[carousel].scrollLeft)
+        } else {
+            carousels[carousel].scrollLeft = 0;
+            temp_dict[carousel] = []
+        }
     }
 }
+
+let t = setInterval(() => {
+    scrollRight();
+}, 12);
